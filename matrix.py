d371a0fadf6500b406fd12c118c8ae2f1d237c27
den3yb <den3yb@gmail.com>
2023-10-31 01:20:29 +0400

    Why doesnt work

--- matrix.py
+++ matrix.py
@@ -1,17 +1,40 @@
+from multiprocessing import Pool
+
+s=0
+c=0
+mat = [0]
+
+def create(_s, _c):
+    global s
+    global c
+    global mat
+    s=_s
+    c=_c
+    _mat = []
+    for i in range (s):
+        m=[]
+        for j in range (c):
+            m.append(int(input()))
+        _mat.append(m)
+    mat=_mat
+    return mat
+
+def printf():
+    for i in range (s):
+        for j in range (c):
+            print(mat[i][j], end = " ")
+        print()
 
 
+def parsumm():
+    with Pool(s) as p:
+        st=[]
+        for i in range (s):
+            st.append(i)
+        print(p.map(summ,st))
 
-s = int(input("Ввеедите количество рядов"))  
-c = int(input("Введите количество столбцов"))  
-matrix = []
-for i in range (s):
-    m=[]
-    for j in range (c):
-        m.append(int(input()))
-    matrix.append(m)
-
-for i in range (s):
-    for j in range (c):
-        print(matrix[i][j], end = " ")
-    print()
-
+def summ(x):
+    sumtemp = 0
+    for i in range (c):
+        sumtemp+=mat[x][i]
+    return sumtemp
--- multyp.py
+++ multyp.py
@@ -1,21 +1,9 @@
 from multiprocessing import Pool
-
-mat = [[1, 1, 1],  
-      [2, 2, 2],  
-      [3, 3, 3]] 
-
-s = 3
-c = 3
-
-def f(x):
-    sumtemp = 0
-    for i in range (c):
-        sumtemp+=mat[x][i]
-    return sumtemp
+from matrix import *
 
 if __name__ == '__main__':
-    with Pool(s) as p:
-        st=[]
-        for i in range (s):
-            st.append(i)
-        print(p.map(f,st))
+    s=int(input("Ваедите число строк"))
+    c=int(input("Ваедите число столбцов"))
+    m=create(s, c)
+    printf()
+    parsumm()
